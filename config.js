44a874a98fa7a0307036be6eac2c174a360eaba4
BakeryBeats <elroyopaffiliate@gmail.com>
2026-03-11 23:58:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,19 @@
-const API_URL = "https://your-api-endpoint.com/chat";
+async function fetchAI(question){
+
+let response = await fetch(
+"https://hook.eu1.make.com/4o4be72uc2ov8ygf5vyalydygpmtb0d0",
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+question:question
+})
+});
+
+let data = await response.json();
+
+return data.answer;
+
+}
